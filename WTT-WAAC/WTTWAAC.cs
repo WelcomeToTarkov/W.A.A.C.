d284c7b6ac7606ddf9fb0a04b0b75112749a1e35
GrooveypenguinX <jakechase2110@gmail.comX>
2025-11-06 19:38:03 -0500

    added moddependencies

--- WTT-WAAC/WTTWAAC.cs
+++ WTT-WAAC/WTTWAAC.cs
@@ -16,7 +16,10 @@ public record ModMetadata : AbstractModMetadata
     public override SemanticVersioning.Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version?.ToString(3));
     public override Range SptVersion { get; init; } = new("~4.0.1");
     public override List<string>? Incompatibilities { get; init; }
-    public override Dictionary<string, Range>? ModDependencies { get; init; }
+    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    {
+        { "com.wtt.commonlib", new Range("~2.0.0") }
+    };
     public override string? Url { get; init; }
     public override bool? IsBundleMod { get; init; } = true;
     public override string License { get; init; } = "MIT";
@@ -28,7 +31,6 @@ public class WAAC(
 {
     public async Task OnLoad()
     {
-        
         Assembly assembly = Assembly.GetExecutingAssembly();
         await wttCommon.CustomClothingService.CreateCustomClothing(assembly);
         await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
